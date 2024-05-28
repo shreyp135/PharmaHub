f9faba2cf60c27b47e21c1c8496bc9fe64a66961
Shreyp135 <shreyanshpaliwalcmsmn@gmail.com>
2024-05-28 14:45:58 +0530

    configure tailwind and install necessary npm packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Header from '../components/navbar'
 
 function App() {
 
   return (
     <>
-      <h2>hello</h2>
-    </>
-  )
+       <h2>hello</h2>
+            
+    </> 
+  );
 }
 
 export default App
