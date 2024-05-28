06ab388b4a40479c7a37277ffde82660cd474264
Shreyp135 <shreyanshpaliwalcmsmn@gmail.com>
2024-05-28 15:30:23 +0530

    added database connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+//.env file config
+dotenv.config();
+
+//express app 
 const app = express();
 
+//database connection
+mongoose.connect(process.env.MONGO_URL).then(() =>{
+    console.log("Connected to MongoDB server successfully");
+}).catch((err)=>{
+    console.log(err);
+});
+
+
+//server start
 app.listen(8080,()=>{
     console.log("Server started on port 8080");
 });
+
