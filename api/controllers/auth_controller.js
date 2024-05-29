bf3d2706d0f7836af2a46598b4215b7f178bd863
Shreyp135 <shreyanshpaliwalcmsmn@gmail.com>
2024-05-29 14:54:58 +0530

    added Sign-in routes and functionality

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user_model.js"
-
+import { errorHandler } from "../utils/errorHandler.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req,res,next)=>{
     try{
@@ -12,4 +13,25 @@ export const signup = async (req,res,next)=>{
     } catch (err){
     next(err);
     }
+};
+
+
+export const signin = async (req,res,next)=>{
+    try{
+        const {email, password} =  req.body;
+        const currUser = await User.findOne({email});
+        if (!currUser) 
+            return next(errorHandler(152,"User not found"));
+        const isUser = (currUser.password === password);
+        if(!isUser) 
+            return next(errorHandler(153,"Wrong credentials"));
+        console.log("successs");
+        const token  = jwt.sign({ id: currUser._id }, process.env.JWT_SECRET);
+        const {password: pass, ...cred} = currUser._doc;
+        const expiryDate = new Date(Date.now() + 259200000)//expiry after 3 days
+        res.cookie("access_token", token, {httpOnly: true, expires: expiryDate}).status(200).json(cred);//creating a cookie
+    }   catch (err){
+        next(err);
+        }
+
 };
--- api/routes/auth_route.js
+++ api/routes/auth_route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { signup } from "../controllers/auth_controller.js";
+import { signup, signin } from "../controllers/auth_controller.js";
 
 const router = express.Router();
 
 router.post("/signup",signup);
+router.post("/signin",signin);
+
 
 export default router;
